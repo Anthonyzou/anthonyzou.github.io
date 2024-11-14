d529f9af4599b0545733991b7a55588b286eabb5
Anthony Ou <ou.anthony@gmail.com>
2024-11-14 10:44:27 -0700

    Remove jspdf

--- run.mjs
+++ run.mjs
@@ -6,16 +6,15 @@ import * as sass from "sass";
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const styling = sass.compile("./resume.scss");
-const htmFile = "anthonyou_temp.htm";
-
-const html = pug.renderFile("anthonyou.pug", {
-  pretty: false,
-  styling: styling.css,
-});
-
 puppeteer.launch().then(async (browser) => {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const styling = sass.compile("./resume.scss");
+  const htmFile = "anthonyou_temp.htm";
+
+  const html = pug.renderFile("anthonyou.pug", {
+    pretty: false,
+    styling: styling.css,
+  });
   await fs.writeFile(htmFile, html);
 
   const page = await browser.newPage();
