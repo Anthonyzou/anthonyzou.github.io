e816c14e50c9cd2cde242bd850d0ba623b021e9b
Anthony Ou <ou.anthony@gmail.com>
2024-11-15 16:25:18 -0700

    Add file watching

--- run.mjs
+++ run.mjs
@@ -1,12 +1,16 @@
 import pug from "pug";
 import fs from "fs/promises";
+
 import puppeteer from "puppeteer";
 import * as sass from "sass";
-
+import { debounce } from "lodash-es";
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
-puppeteer.launch().then(async (browser) => {
+const ac = new AbortController();
+
+const fn = async () => {
+  const browser = await puppeteer.launch();
   const __dirname = dirname(fileURLToPath(import.meta.url));
   const styling = sass.compile("./resume.scss");
   const htmFile = "anthonyou_temp.htm";
@@ -21,4 +25,27 @@ puppeteer.launch().then(async (browser) => {
   await page.goto(`file://${__dirname}/${htmFile}`);
   await page.pdf({ path: "anthonyou.pdf" });
   await browser.close();
+};
+const dfn = debounce(fn, 1000);
+
+// Start watching the file
+(async () => {
+  try {
+    const watcher = fs.watch("./", { signal: ac.signal });
+
+    for await (const event of watcher) {
+      if (event.filename.match("pug|scss")) {
+        console.log(`running ${new Date()}`);
+        await dfn();
+      }
+    }
+  } catch (err) {
+    if (err.name === "AbortError") return;
+    throw err;
+  }
+})();
+
+process.on("SIGINT", function () {
+  console.log("sigint");
+  ac.abort();
 });
